80047558b54a70aa9f457e8289bc97ad34c86cb4
acidicoala <67734819+acidicoala@users.noreply.github.com>
2022-02-01 14:03:21 +0200

    Vector typedef

--- src/koalabox.hpp
+++ src/koalabox.hpp
@@ -2,11 +2,15 @@
 
 #include <filesystem>
 #include <string>
+#include <vector>
 
 namespace koalabox {
     typedef std::filesystem::path Path;
     typedef std::string String;
     typedef std::wstring WideString;
+
+    template <typename T>
+    using Vector = std::vector<T>;
 
     [[maybe_unused]]
     extern String project_name;
