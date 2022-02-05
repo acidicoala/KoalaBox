a2477c39735c343cb3e49bec56d878045b9ddaee
acidicoala <67734819+acidicoala@users.noreply.github.com>
2022-02-05 21:13:23 +0200

    Added `version.gen.rc`

--- src/koalabox/koalabox.hpp
+++ src/koalabox/koalabox.hpp
@@ -17,10 +17,8 @@ namespace koalabox {
     template<typename T>
     using Set [[maybe_unused]] = std::set<T>;
 
-
     template<typename K, typename V>
     using Map [[maybe_unused]] = std::map<K, V>;
-
 
     [[maybe_unused]]
     extern String project_name;
