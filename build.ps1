44b0553105af8a7352def9e879f59e1da26bcba6
acidicoala <67734819+acidicoala@users.noreply.github.com>
2023-01-13 03:48:26 +0300

    Updated build script

--- build.ps1
+++ build.ps1
@@ -4,14 +4,25 @@
 #   build.ps1 64 Debug
 #   build.ps1 64 Release
 
-$arch = $args[0]
-if ($arch -notmatch '^(32|64)$') {
+$target = $args[0]
+
+$arch = $args[1]
+if ($arch -notmatch '^(32|64)$')
+{
     throw "Invalid architecute. Expected: '32' or '64'. Got: '$arch'"
 }
-$platform = If ($arch -eq '32') { 'Win32' } Else { 'x64' }
+$platform = If ($arch -eq '32')
+{
+    'Win32'
+}
+Else
+{
+    'x64'
+}
 
-$config = $args[1]
-if ($config -notmatch '^(Debug|Release|RelWithDebInfo)$') {
+$config = $args[2]
+if ($config -notmatch '^(Debug|Release|RelWithDebInfo)$')
+{
     throw "Invalid architecute. Expected: 'Debug' or 'Release' or 'RelWithDebInfo'. Got: '$config'"
 }
 
@@ -20,5 +31,5 @@ $Env:BUILD_DIR = "build\$arch"
 function Build-Project {
     cmake -G "Visual Studio 17 2022" -A $platform -B "$Env:BUILD_DIR" "$Env:CMAKE_OPTIONS"
 
-    cmake --build "$Env:BUILD_DIR" --config $config  "$Env:CMAKE_BUILD_OPTIONS"
+    cmake --build "$Env:BUILD_DIR" --target $target --config $config
 }
